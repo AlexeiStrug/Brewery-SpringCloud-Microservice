07ae85c3461de79eed9425ba14774e6199dea26f
alexei_strug <alexeistrug@gmail.com>
2020-07-12 20:27:37 +0200

    added docker files and maven configuration for docker plugin

---
+++ beer-order-service/target/generated-sources/annotations/com/example/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,28 @@
+package com.example.beer.order.service.web.mappers;
+
+import com.example.beer.order.service.domain.BeerOrderLine;
+import com.example.brewery.model.BeerOrderLineDto;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-07-12T20:16:45+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (JetBrains s.r.o)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerOrderLineMapper delegate;
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto)  {
+        return delegate.dtoToBeerOrderLine( dto );
+    }
+}

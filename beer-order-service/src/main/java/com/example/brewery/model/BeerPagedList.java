f2d0395a82450a8625190d199f4335cd0e96128b
alexei_strug <alexeistrug@gmail.com>
2020-07-12 18:51:47 +0200

    added config-rep for all services

--- beer-order-service/src/main/java/com/example/brewery/model/BeerPagedList.java
+++ beer-order-service/src/main/java/com/example/brewery/model/BeerPagedList.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+
 import java.io.Serializable;
 import java.util.List;
 
